12a9523d74c2c9331774e17027d8ef2a469c54ec
Dave Cockrell <dave@telicent.io>
2026-01-21 11:52:09 +0000

    fix(create-yarnrc-file): add custom fontawesome kit registry
    Uses the the same key as fa pro

--- .github/actions/yarn-prepare/create-yarnrc-file.sh
+++ .github/actions/yarn-prepare/create-yarnrc-file.sh
@@ -2,5 +2,6 @@
 
 npm config set "@telicent-io:registry" "https://npm.pkg.github.com/"
 npm config set "//npm.pkg.github.com/:_authToken" $PACKAGE_PAT
+npm config set "@awesome.me:registry" https://npm.fontawesome.com/
 npm config set "@fortawesome:registry" https://npm.fontawesome.com/
 npm config set  "//npm.fontawesome.com/:_authToken" $FONT_AWESOME_KEY
